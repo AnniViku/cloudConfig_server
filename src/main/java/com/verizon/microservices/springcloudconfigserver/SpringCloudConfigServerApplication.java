1abf4e55dc3c0f7a693fc36575dcb917addbd5d7
AnniViku <vikashannikumar@gmail.com>
2022-09-11 07:44:56 -0400

    commited cloud config server

---
+++ src/main/java/com/verizon/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.verizon.microservices.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+
+@EnableConfigServer    //Connect Spring Cloud Config Server to Local Git Repository
+@SpringBootApplication
+public class SpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+	}
+
+}
